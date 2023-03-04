eb3fe4474d4090f335c7bf0d3a54ce5f4b46175f
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-03-04 21:37:25 +0100

    Assertions about expected exceptions

--- test_sample.py
+++ test_sample.py
@@ -1,5 +1,30 @@
+import pytest
+
+# WRITING AND REPORTING ASSERTATIONS IN TESTS
+
+
+
+# Asserting with the assert statement(used to cerify expectations and values in Python)
 def func(x):
     return x + 3
 
 def test_answer():
-    assert func(4) == 10
+    assert func(4) == 7
+
+
+
+# Assertions about expected exceptions
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError): # a built-in Python exception thrown when a number is divided by 0.
+        2/0
+
+
+def test_recursion_depth():
+    with pytest.raises(RuntimeError) as excinfo:
+
+        def f():
+            f()
+
+        f()
+    assert "maximum recursion" in str(excinfo.value) # if you need to have access to the actual exception info (.type, .value, .traceback)
