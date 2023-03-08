d6d81a71b2d442a87b90398106e8e37247940196
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-03-08 17:20:13 +0100

    Fixure errors

--- Fixtures/test_fixture.py
+++ Fixtures/test_fixture.py
@@ -19,3 +19,36 @@ def fruit_basket(my_fruit):
 
 def test_my_fruit_in_basket(my_fruit, fruit_basket):
     assert my_fruit in fruit_basket
+
+
+
+# Fixture errors
+
+@pytest.fixture
+def order():
+    return []
+
+
+@pytest.fixture
+def append_first(order):
+    order.append(1)
+
+
+@pytest.fixture
+def append_second(order, append_first):
+    order.extend([2])
+
+
+@pytest.fixture(autouse=True)
+def append_third(order, append_second):
+    order += [3]
+
+
+def test_order(order):
+    assert order == [1, 2, 3]
+
+# If, for whatever reason, order.append(1) had a bug and it raises an exception, 
+# we wouldn’t be able to know if order.extend([2]) or order += [3] would also have problems. 
+# After append_first throws an exception, pytest won’t run any more fixtures for test_order, 
+# and it won’t even try to run test_order itself. 
+# The only things that would’ve run would be order and append_first
