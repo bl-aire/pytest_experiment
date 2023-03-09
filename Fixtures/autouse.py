bc1f2cc2cfa6b4fe30a0885b3546713d5212168c
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-03-09 20:34:44 +0100

    Autouse fixture explanation

--- Fixtures/autouse.py
+++ Fixtures/autouse.py
@@ -17,9 +17,9 @@ def append_first(order, first_entry):
 
 
 def test_string_only(order, first_entry):
-    assert order == [first_entry]
+    assert order == [first_entry] # Autouse” fixtures are a convenient way to make all tests automatically request them...(Array containing first order is returned in the auto use fixture but others after are affected though neither test requested it. THEY CAN BE REQUESTED )
 
 
 def test_string_and_int(order, first_entry):
     order.append(2)
-    assert order == [first_entry, 2]
+    assert order == [first_entry, 2] # Autouse” fixtures are a convenient way to make all tests automatically request them
