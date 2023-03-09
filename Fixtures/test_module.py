d037b85b0df688f47866fb2449c20cfd237acd2f
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-03-09 21:33:43 +0100

    Scope example

---
+++ Fixtures/test_module.py
@@ -0,0 +1,14 @@
+def test_ehlo(smtp_connection):
+    response, msg = smtp_connection.ehlo()
+    assert response == 250
+    assert b"smtp.gmail.com" in msg
+    assert 0  # for demo purposes
+
+
+def test_noop(smtp_connection):
+    response, msg = smtp_connection.noop()
+    assert response == 250
+    assert 0 # for demo purposes
+
+
+# TimeoutError: timed out
